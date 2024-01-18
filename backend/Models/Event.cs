ca2299688572c7be5d94b528df796f6bc8e4b617
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-18 14:24:54 +0100

    remove redundant model fields

--- backend/Models/Event.cs
+++ backend/Models/Event.cs
@@ -1,12 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace EventFider.Models;
+namespace EventFinder.Models;
 
 public class Event
 {
     [Key]
     public int Id { get; set; }
     public string Title { get; set; } = default!;
+    public string Category { get; set; } = default!;
     public string Description { get; set; } = default!;
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
@@ -15,6 +16,4 @@ public class Event
     public double Latitude { get; set; }
     public double Longitude { get; set; }
     public decimal Price { get; set; }
-    public string Category { get; set; } = default!;
-    public string ImageUrl { get; set; } = default!;
 }
