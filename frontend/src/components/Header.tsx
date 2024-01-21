8b4b1005cc69ff943a5963efeac0d83b40716f52
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-21 20:22:30 +0100

    heighten oppacity of card images

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="navbar bg-base-100 self-start shadow">
-      <Link to="/home" className="btn btn-ghost text-xl text-opacity-100 z-50">
+      <Link to="/home" className="btn btn-ghost text-xl z-50">
         Event Eagle
       </Link>
     </div>
