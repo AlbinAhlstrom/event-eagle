32713689275756608e9a5982bd788aa8201f1e84
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-25 22:24:58 +0100

    fix opacity

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
         }}
       >
         {" "}
-        <div className="hero-overlay bg-opacity-70"></div>
+        <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
