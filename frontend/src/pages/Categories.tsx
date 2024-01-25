ef482f5dac30ddab1c0b02cb977feac96646b5cd
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-25 22:30:23 +0100

    make page responsive

--- frontend/src/pages/Categories.tsx
+++ frontend/src/pages/Categories.tsx
@@ -1,34 +1,41 @@
 import { useNavigate } from "react-router-dom";
-import Header from '../components/Header';
-import CategoryCard from '../components/CategoryCard';
-import musicImage from '../images/music-events.jpg'
-import sportsImage from '../images/sport-events.jpg'
-import theatreImage from '../images/theatre-events.jpg'
-import familyImage from '../images/family-events.jpg'
-
+import Header from "../components/Header";
+import CategoryCard from "../components/CategoryCard";
+import musicImage from "../images/music-events.jpg";
+import sportsImage from "../images/sport-events.jpg";
+import theatreImage from "../images/theatre-events.jpg";
+import familyImage from "../images/family-events.jpg";
 
 function Categories() {
   const eventCategories = [
-    {name: "Music", image:musicImage}, 
-    {name: "Sports", image:sportsImage},
-    {name: "Arts", image:theatreImage}, 
-    {name: "Family",  image:familyImage}
-  ]
-  const navigate = useNavigate()
+    { name: "Music", image: musicImage },
+    { name: "Sports", image: sportsImage },
+    { name: "Arts", image: theatreImage },
+    { name: "Family", image: familyImage },
+  ];
+  const navigate = useNavigate();
 
   return (
     <>
-      <Header/>
-      <div className='flex flex-col gap-10 items-center justify-center mt-10'>
-      <section className='grid grid-cols-1 grid-rows-4 gap-4 align-middle'>
-        {eventCategories.map( category => {
-      return <CategoryCard category={category.name}  image={category.image}/>
-        })}
-      </section>
-      <button className="btn btn-primary" onClick={() => navigate("/events")}>Show all Categories</button>
+      <Header />
+      <div className="flex flex-col gap-10 items-center justify-center mt-10">
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 align-middle">
+          {eventCategories.map((category, index) => {
+            return (
+              <CategoryCard
+                key={index}
+                category={category.name}
+                image={category.image}
+              />
+            );
+          })}
+        </section>
+        <button className="btn btn-primary" onClick={() => navigate("/events")}>
+          Show all Categories
+        </button>
       </div>
     </>
-  )
+  );
 }
 
-export default Categories
+export default Categories;
