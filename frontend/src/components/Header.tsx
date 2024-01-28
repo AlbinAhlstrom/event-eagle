8c2a71f9140cd7edf17fd5b6037fa47b2b249577
Albin Ahlström <ahlstrom.albin@gmail.com>
2024-01-28 16:12:34 +0100

    add logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import logo from '../images/event-eagle-logo-nobg.webp';
 
 const Header = () => {
   return (
     <div
       className="navbar bg-base-100 self-start shadow h-nav z-50 sticky top-0"
     >
-      <Link to="/home" className="btn btn-ghost text-xl">
+      <Link to="/home" className="btn btn-ghost text-xl h-full">
+      <img src={logo} className="h-full"></img>
+
         Event Eagle
       </Link>
     </div>
