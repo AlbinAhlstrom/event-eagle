52e732b39da000dda448d688eb3845d02f74c53e
Albin Ahlström <ahlstrom.albin@gmail.com>
2024-01-28 15:36:03 +0100

    remove daisy ui as cdn and add it as plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
+import daisyui from 'daisyui';
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  plugins: [daisyui],
   theme: {
     extend: {
       spacing: {
