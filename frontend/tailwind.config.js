7ba25b84847fca41c7bbca3dbf6bf97031f4f15c
Albin Ahlström <ahlstrom.albin@gmail.com>
2024-01-28 15:44:27 +0100

    add forest theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,36 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  daisyui: {
+    themes: [
+      "light", 
+      "dark", 
+      "cupcake",
+      "forest",
+      {
+        mytheme: {
+        
+"primary": "#4ade80",
+        
+"secondary": "#86efac",
+        
+"accent": "#16a34a",
+        
+"neutral": "#1f2937",
+        
+"base-100": "#1f2937",
+        
+"info": "#facc15",
+        
+"success": "#0f766e",
+        
+"warning": "#831843",
+        
+"error": "#9f1239",
+        },
+      },
+    ],
+  },
   plugins: [daisyui],
   theme: {
     extend: {
