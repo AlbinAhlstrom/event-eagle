11d830c6ff82d1bc6e5e556403134f7146f4fe4f
ayoubzann <ayoubzann@gmail.com>
2024-01-29 11:48:47 +0100

    Added BrowserRouter config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import Header from './components/Header.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import Header from "./components/Header.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Header/>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Header />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
