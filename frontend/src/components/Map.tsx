f2c1b928f0f23f3c1428f4a8f7d270c000567e2a
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-29 21:20:13 +0100

    add api key as env var

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -5,14 +5,14 @@ import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react
 const MapWindow = () => {
 
   const position = {lat: 59.34676644462517, lng: 18.055573862709853}
-
+  const apiUrl = import.meta.env.VITE_GMAPS_KEY;
   return (
-    <APIProvider apiKey={process.env.REACT_APP_GMAPS_API_KEY}>
+    <APIProvider apiKey={apiUrl}>
       <div className="h-40vh w-40vh">
-        <Map zoom={9} center={position}></Map>
+
       </div>
     </APIProvider>
-  );
-};
+  )
+}
 
 export default MapWindow;
