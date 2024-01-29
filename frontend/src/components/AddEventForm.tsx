1adebcafc46083c98ba1ce85ff2f1657ef80f667
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-29 14:07:29 +0100

    add form and admin dashboard

---
+++ frontend/src/components/AddEventForm.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { EventListing, defaultEventListing } from "../util";
+
+interface EventFormProps {
+  postEvent: (event: EventListing) => Promise<void>;
+}
+
+const AddEventForm: React.FC<EventFormProps> = ({ postEvent }) => {
+  const [EventListing, setEventListing] =
+    useState<EventListing>(defaultEventListing);
+
+  const handleChange = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    setEventListing({ ...EventListing, [name]: value });
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    await postEvent({
+      ...EventListing,
+    });
+    // Reset form after submission
+    setEventListing(defaultEventListing);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Title:
+          <input
+            type="text"
+            name="title"
+            className="input input-bordered input-ghost"
+            value={EventListing.title}
+            onChange={handleChange}
+            required
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Description:
+          <textarea
+            name="description"
+            className="textarea textarea-bordered"
+            value={EventListing.description}
+            onChange={handleChange}
+            required
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Venue:
+          <input
+            type="text"
+            name="venue"
+            className="input input-bordered input-ghost"
+            value={EventListing.venue}
+            onChange={handleChange}
+            required
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Address:
+          <input
+            type="text"
+            name="address"
+            className="input input-bordered input-ghost"
+            value={EventListing.address}
+            onChange={handleChange}
+            required
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Price:
+          <input
+            type="number"
+            name="price"
+            className="input input-bordered"
+            value={EventListing.price.toString()}
+            onChange={handleChange}
+            required
+          />
+        </label>
+      </div>
+      <div>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Category:</span>
+          </div>
+          <input
+            type="text"
+            name="category"
+            className="input"
+            value={EventListing.category}
+            onChange={handleChange}
+            required
+          />{" "}
+        </label>
+      </div>
+      <div>
+        <button type="submit">Create Event</button>
+      </div>
+    </form>
+  );
+};
+
+export default AddEventForm
--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,8 +1,73 @@
+import { useEffect, useState } from "react";
+import { EventListing } from "../util";
+import AddEventForm from "../components/AddEventForm";
 
-const AdminDashboard = () => {
+export const AdminDashboard = () => {
+  const [events, setEvents] = useState<EventListing[]>([]);
+  const [isCreating, setIsCreating] = useState<boolean>(false);
+  const BASE_URL = "https://event-eagle.azurewebsites.net";
+
+  // Get all events
+  useEffect(() => {
+    const EVENTS_ENDPOINT = `${BASE_URL}/Events`;
+    fetch(EVENTS_ENDPOINT)
+      .then((response) => response.json())
+      .then((data: EventListing[]) => setEvents(data));
+  }, []);
+
+  // Post an event
+  const postEvent = async (event: Omit<EventListing, "id">) => {
+    const EVENTS_ENDPOINT = `${BASE_URL}/Events`;
+    const response = await fetch(EVENTS_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(event),
+    });
+    const data: EventListing = await response.json();
+    setEvents([...events, data]);
+    // Close the form after posting the new event
+    setIsCreating(false);
+  };
+
+  // Delete an event
+  const deleteEvent = async (id: number) => {
+    const EVENTS_ENDPOINT = `${BASE_URL}/Events/${id}`;
+    const response = await fetch(EVENTS_ENDPOINT, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      setEvents(events.filter((event) => event.id !== id));
+    }
+  };
+
+  const handleCreateEvent = () => {
+    setIsCreating(true);
+  };
+
   return (
-    <div></div>
-  )
-}
+    <div>
+      <h1>Events</h1>
+      <ul>
+        {events.map((event) => (
+          <li key={event.id}>
+            {event.title}
+            <button className="btn" onClick={() => deleteEvent(event.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      {isCreating ? (
+        <AddEventForm postEvent={postEvent} />
+      ) : (
+        <button className="btn" onClick={handleCreateEvent}>
+          Create New Event
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
