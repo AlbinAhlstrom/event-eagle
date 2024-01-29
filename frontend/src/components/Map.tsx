5baf7efd8820aae0f264985dcb316698bebf387e
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-29 21:19:17 +0100

    add types/node

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+"useclient";
+import {useState} from "react";
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
 
-const Map = () => {
+const MapWindow = () => {
+
+  const position = {lat: 59.34676644462517, lng: 18.055573862709853}
+
   return (
-    <div>
-      <iframe src="https://www.google.com/maps/@59.3512593,18.0413688,13.5z?entry=ttu" width="600" height="450" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
+    <APIProvider apiKey={process.env.REACT_APP_GMAPS_API_KEY}>
+      <div className="h-40vh w-40vh">
+        <Map zoom={9} center={position}></Map>
+      </div>
+    </APIProvider>
   );
 };
 
-export default Map;
+export default MapWindow;
