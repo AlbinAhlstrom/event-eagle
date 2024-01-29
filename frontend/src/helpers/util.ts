a01943363b0665df234b16986d2c7e6e2258e122
ayoubzann <ayoubzann@gmail.com>
2024-01-29 12:31:42 +0100

    Added utilites from other files

--- frontend/src/helpers/util.ts
+++ frontend/src/helpers/util.ts
@@ -1,4 +1,4 @@
-const getDistanceFromLatLonInKm = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
+export const getDistanceFromLatLonInKm = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
     // Radius of the earth in km
     const R = 6371;
     const dLat = deg2rad(lat2 - lat1);
@@ -13,8 +13,52 @@ const getDistanceFromLatLonInKm = (lat1: number, lon1: number, lat2: number, lon
     return d;
 };
 
-const deg2rad = (deg: number): number => {
+export const deg2rad = (deg: number): number => {
     return deg * (Math.PI / 180);
 };
 
-export default getDistanceFromLatLonInKm
+export type EventListing = {
+    id: number;
+    title: string;
+    description: string;
+    startTime: Date;
+    endTime: Date;
+    venue: string;
+    address: string;
+    latitude: number;
+    longitude: number;
+    price: number;
+    category: "Music" | "Sports" | "Arts" | "Family";
+};
+
+export const defaultEventListing: EventListing = {
+    id: 0,
+    title: "",
+    description: "",
+    startTime: new Date(),
+    endTime: new Date(),
+    venue: "",
+    address: "",
+    latitude: 0,
+    longitude: 0,
+    price: 0,
+    category: "Music"
+};
+
+export type EventCardProps = {
+    id: number;
+    title: string;
+    description: string;
+    startTime: Date;
+    venue: string;
+    price: number;
+    category: 'Music' | 'Sports' | 'Arts' | 'Family';
+  };
+  
+export interface CountdownTimerProps {
+    targetDate: Date;
+  }
+
+export interface IconMap {
+    [key: string]: string;
+  }
