9c36e1b5862032ddf4a9e383fc87982d275770d6
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-29 20:46:12 +0100

    install vis.gl react-google-maps

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,82 +1,11 @@
+import React from "react";
 
-function Map(): void {
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 4,
-      center: { lat: -24.345, lng: 134.46 }, // Australia.
-    }
+const Map = () => {
+  return (
+    <div>
+      <iframe src="https://www.google.com/maps/@59.3512593,18.0413688,13.5z?entry=ttu" width="600" height="450" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
   );
-
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer({
-    draggable: true,
-    map,
-    panel: document.getElementById("panel") as HTMLElement,
-  });
-
-  directionsRenderer.addListener("directions_changed", () => {
-    const directions = directionsRenderer.getDirections();
-
-    if (directions) {
-      computeTotalDistance(directions);
-    }
-  });
-
-  displayRoute(
-    "Perth, WA",
-    "Sydney, NSW",
-    directionsService,
-    directionsRenderer
-  );
-}
-
-function displayRoute(
-  origin: string,
-  destination: string,
-  service: google.maps.DirectionsService,
-  display: google.maps.DirectionsRenderer
-) {
-  service
-    .route({
-      origin: origin,
-      destination: destination,
-      waypoints: [
-        { location: "Adelaide, SA" },
-        { location: "Broken Hill, NSW" },
-      ],
-      travelMode: google.maps.TravelMode.DRIVING,
-      avoidTolls: true,
-    })
-    .then((result: google.maps.DirectionsResult) => {
-      display.setDirections(result);
-    })
-    .catch((e) => {
-      alert("Could not display directions due to: " + e);
-    });
-}
-
-function computeTotalDistance(result: google.maps.DirectionsResult) {
-  let total = 0;
-  const myroute = result.routes[0];
-
-  if (!myroute) {
-    return;
-  }
-
-  for (let i = 0; i < myroute.legs.length; i++) {
-    total += myroute.legs[i]!.distance!.value;
-  }
-
-  total = total / 1000;
-  (document.getElementById("total") as HTMLElement).innerHTML = total + " km";
-}
-
-declare global {
-  interface Window {
-    initMap: () => void;
-  }
-}
-window.initMap = initMap;
+};
 
 export default Map;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Map from "../components/Map";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -9,7 +10,6 @@ const Home = () => {
 
         style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}
       >
-        {" "}
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
@@ -24,6 +24,7 @@ const Home = () => {
             >
               Browse Categories
             </button>
+            <Map/>
           </div>
         </div>
       </div>
