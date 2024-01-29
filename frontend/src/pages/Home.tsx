56edd4750280ac7ab0a2631c1fb16770a0c61294
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-29 21:31:17 +0100

    remove redundant map import

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Map from "../components/Map";
+
 
 const Home = () => {
   const navigate = useNavigate();
@@ -7,16 +7,18 @@ const Home = () => {
     <>
       <div
         className="hero h-screenH"
-
-        style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}
+        style={{
+          backgroundImage:
+            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+        }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
             <p className="mb-5 text-xl">
-              Find and grab tickets to today's nearby events. Tap below to
-              start your next adventure!
+              Find and grab tickets to today's nearby events. Tap below to start
+              your next adventure!
             </p>
             <button
               className="btn btn-primary"
