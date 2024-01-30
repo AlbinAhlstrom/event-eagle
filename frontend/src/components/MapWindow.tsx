98eb64fbf8a7629488a94df03a89d760b2d1bd38
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-30 09:40:37 +0100

    fix syntax

--- frontend/src/components/MapWindow.tsx
+++ frontend/src/components/MapWindow.tsx
@@ -6,16 +6,15 @@ const MapWindow = () => {
   const position = {lat: 59.34676644462517, lng: 18.055573862709853}
   const apiUrl = import.meta.env.VITE_GMAPS_KEY;
   return (
-    <APIProvider apiKey={apiUrl}>
+    <div>
+<APIProvider apiKey={apiUrl}>
       <div className="h-40vh w-40vh">
         <Map 
         zoom={12} 
         center={position}
         styles={mapStyle}
-        options={{
-          scrollwheel: false,
-          gestureHandling: 'none',
-        }}
+        scrollwheel = {false}
+        gestureHandling = {'none'}
         >
           <AdvancedMarker position={position}>
             <Pin/>
@@ -23,6 +22,8 @@ const MapWindow = () => {
         </Map>
       </div>
     </APIProvider>
+    </div>
+    
   )
 }
 
