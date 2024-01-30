a3459a73ed119c4e43839de8a714697c305a76a6
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-30 13:15:32 +0100

    add draggability to pin

--- frontend/src/components/MapWindow.tsx
+++ frontend/src/components/MapWindow.tsx
@@ -1,31 +1,52 @@
-import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
-import { mapStyle } from "../util";
+import React, { useState, useEffect } from 'react';
+import { APIProvider, Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
 
-const MapWindow = () => {
-
-  const position = {lat: 59.34676644462517, lng: 18.055573862709853}
+const MapWindow: React.FC = () => {
+  const [position, setPosition] = useState({lat: 59.34676644462517, lng: 18.055573862709853});
   const apiUrl = import.meta.env.VITE_GMAPS_KEY;
+  const mapId = import.meta.env.VITE_GMAPS_MAPID;
+
+  const handleDragEnd = (event: google.maps.MapMouseEvent) => {
+    const newPos = event.latLng;
+    if (newPos) {
+      setPosition({ lat: newPos.lat(), lng: newPos.lng() });
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setPosition({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }, []);
+
   return (
     <div className="flex justify-center items-center h-screen-h">
-<APIProvider apiKey={apiUrl}>
-      <div className="h-80vh w-80vh">
-        <Map 
-        zoom={12} 
-        center={position}
-        styles={mapStyle}
-        scrollwheel = {false}
-        gestureHandling = {'none'}
-        disableDefaultUI={true}
-        >
-          <AdvancedMarker position={position}>
-            <Pin/>
+      <APIProvider apiKey={apiUrl}>
+        <Map
+          zoom={16}
+          center={position}
+          mapId={mapId}
+          gestureHandling={'none'}
+          disableDefaultUI={true}>          
+          <AdvancedMarker
+            position={position}
+            draggable={true}
+            onDragEnd={handleDragEnd}
+          >
+            <Pin />
           </AdvancedMarker>
         </Map>
-      </div>
-    </APIProvider>
+      </APIProvider>
     </div>
-    
   )
-}
+};
 
 export default MapWindow;
