ff9240aef9da34fbe874cc3ceae608d5b62452d0
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-30 15:18:00 +0100

    add map to event detail view

--- frontend/src/pages/EventDetails.tsx
+++ frontend/src/pages/EventDetails.tsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import image from '../images/nature.jpg'
 import { EventListing, defaultEventListing } from '../util';
+import MapWindow from '../components/MapWindow';
 
 const EventDetails = () => {
     const { id } = useParams<'id'>(); 
     const [event, setEvent] = useState<EventListing>(defaultEventListing);
     const [startTime, setStartTime] = useState("");
+    const [position, setPosition] = useState({lat:event.latitude, lng:event.longitude})
 
     useEffect(() => {
         const fetchEvent = async () => {
@@ -37,12 +39,16 @@ const EventDetails = () => {
 
     return (
         <>
+        
             <div
                 className="hero min-h-screen"
                 style={{
                     backgroundImage: `url(${image})`,
                 }}
             >
+                <span className='h-2/6 w-2/6 absolute top-20'>
+                        <MapWindow position={position} setPosition={setPosition}/>
+                        </span>
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content glass rounded-md">
                     <div className="max-w-md">
@@ -51,11 +57,14 @@ const EventDetails = () => {
                         <p className="mb-5 text-xl">{event.description}</p>
                         <p>{event.startTime.toString()}</p>
                         <p>{startTime}</p>
+                        <p>{event.latitude}</p>
+                        <p>{event.longitude}</p>
+                        <p>{startTime}</p>
 
                         <h2 className='my-6 text-xl'>Price: {event.price} SEK</h2>
 
                         <p>{event.address}</p>
-                        <p>venue: {event.venue}</p>
+                        
 
                     </div>
                 </div>
