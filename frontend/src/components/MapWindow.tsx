241c5c7abc9b8b92feb19960f1ebe0cc943bb175
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-30 09:35:53 +0100

    add map styel

--- frontend/src/components/MapWindow.tsx
+++ frontend/src/components/MapWindow.tsx
@@ -1,21 +1,25 @@
-"useclient";
-import {useState} from "react";
 import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
+import { mapStyle } from "../util";
 
 const MapWindow = () => {
 
   const position = {lat: 59.34676644462517, lng: 18.055573862709853}
   const apiUrl = import.meta.env.VITE_GMAPS_KEY;
-  const mapId = import.meta.env.VITE_GMAPS_MAPID;
   return (
     <APIProvider apiKey={apiUrl}>
       <div className="h-40vh w-40vh">
         <Map 
-        zoom={9} 
+        zoom={12} 
         center={position}
-        mapId={mapId}
+        styles={mapStyle}
+        options={{
+          scrollwheel: false,
+          gestureHandling: 'none',
+        }}
         >
-          <AdvancedMarker position={position}></AdvancedMarker>
+          <AdvancedMarker position={position}>
+            <Pin/>
+          </AdvancedMarker>
         </Map>
       </div>
     </APIProvider>
