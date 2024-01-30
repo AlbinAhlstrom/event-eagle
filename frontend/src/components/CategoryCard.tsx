8b14dbb4cb41baa061a698fd17644dca0f30df30
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-30 23:26:47 +0100

    limit catagory card width

--- frontend/src/components/CategoryCard.tsx
+++ frontend/src/components/CategoryCard.tsx
@@ -11,10 +11,10 @@ const CategoryCard: React.FC<CategoryCardProps> = (props) => {
 
   return (
     <div 
-    className="card bg-primary shadow-xl image-full max-h-40vh"
+    className="card shadow-xl bg-neutral max-h-40vh max-w-40vw"
     >
   <figure><img src={props.image} alt={props.category} className='w-full'/></figure>
-  <div className="card-body flex justify-between">
+  <div className="card-body flex justify-between h-10">
     <h2 className="card-title">{props.category}</h2>
       <button className="btn btn-primary self-center" onClick={() => navigate("/events/" + props.category)}>Browse</button>
   </div>
