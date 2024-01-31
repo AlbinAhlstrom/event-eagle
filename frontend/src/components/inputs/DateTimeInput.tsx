73df656a264a369a7328cde4a17e996015f065c8
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-31 11:17:04 +0100

    add DateTime input component

---
+++ frontend/src/components/inputs/DateTimeInput.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+type dateTimeInputProps = {
+    title: string,
+    name: string,
+    value: Date,
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const DateTimeInput: React.FC<dateTimeInputProps> = ({title, name, value, onChange}) => {
+  return (
+    <div>
+      <label>
+        <div className="label">
+          <span className="label-text">{title}</span>
+        </div>
+        <input
+          type="datetime-local"
+          name={name}
+          className="input input-bordered input-ghost"
+          value={value.toString()}
+          onChange={onChange}
+          required
+        />
+      </label>
+      ;
+    </div>
+  );
+};
+
+export default DateTimeInput;
