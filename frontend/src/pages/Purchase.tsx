aff7f901e543d471c769fc1cbe47382347f4233f
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-31 09:51:09 +0100

    add purchase page

---
+++ frontend/src/pages/Purchase.tsx
@@ -0,0 +1,33 @@
+import {useState, useEffect} from 'react'
+import { EventListing, defaultEventListing } from '../util';
+import { useParams } from 'react-router-dom';
+
+const Purchase = () => {
+    const { id } = useParams<"id">();
+    const BASE_URL = "https://event-eagle.azurewebsites.net";
+    const [event, setEvent] = useState<EventListing>(defaultEventListing);
+
+    useEffect(() => {
+    const fetchEvent = async () => {
+      const EVENTS_ENDPOINT = `${BASE_URL}/Events/${id}`;
+      try {
+        const response = await fetch(EVENTS_ENDPOINT);
+        const data: EventListing = await response.json();
+        setEvent(data);
+      } catch (error) {
+        console.error("Failed to fetch event", error);
+      }
+    };
+
+    fetchEvent();
+  }, [id, BASE_URL]);
+  return (
+    <div>
+        <div>Purchase</div>
+        <div>{event.id}</div>
+        <div>{event.title}</div>
+    </div>
+  )
+}
+
+export default Purchase
