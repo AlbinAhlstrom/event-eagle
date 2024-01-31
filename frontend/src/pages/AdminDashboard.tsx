a21f9d410d8307bdfbb74e3e8bd9ef09687854d1
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-31 16:26:22 +0100

    use routes

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,21 +1,19 @@
 import { useEffect, useState } from "react";
-import { EventListing } from "../util";
-import AddEventForm from "../components/EventForm";
+import { EventListing, defaultEventListing } from "../util";
+import EventForm from "../components/EventForm";
 import BottomButton from "../components/BottomButton";
 import { useClerk } from "@clerk/clerk-react";
-import { useNavigate } from 'react-router-dom';
 import EditEventCard from "../components/EditEventCard";
+import { useNavigate } from "react-router-dom";
 
 
 export const AdminDashboard = () => {
   const [events, setEvents] = useState<EventListing[]>([]);
-  const [isCreating, setIsCreating] = useState<boolean>(false);
-  const BASE_URL = "http://event-eagle.azurewebsites.net";
+  const {user} = useClerk();
+  const navigate = useNavigate();
 
+  const BASE_URL = "http://event-eagle.azurewebsites.net"
 
-const {user} = useClerk();
-
-  // Get all events
   useEffect(() => {
     const EVENTS_ENDPOINT = `${BASE_URL}/Events`;
     fetch(EVENTS_ENDPOINT)
@@ -23,51 +21,16 @@ const {user} = useClerk();
       .then((data: EventListing[]) => setEvents(data));
   }, []);
 
-  // Post an event
-  const postEvent = async (event: Omit<EventListing, "id">) => {
-    const EVENTS_ENDPOINT = `${BASE_URL}/Events`;
-    const response = await fetch(EVENTS_ENDPOINT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(event),
-    });
-    const data: EventListing = await response.json();
-    setEvents([...events, data]);
-    // Close the form after posting the new event
-    setIsCreating(false);
-  };
-
-  // Delete an event
-  const deleteEvent = async (id: number) => {
-    const EVENTS_ENDPOINT = `${BASE_URL}/Events/${id}`;
-    const response = await fetch(EVENTS_ENDPOINT, {
-      method: "DELETE",
-    });
-    if (response.ok) {
-      setEvents(events.filter((event) => event.id !== id));
-    }
-  };
-
-  const updateEvent = (event: EventListing) => {
-    setIsCreating(true);
-    event
-  }
 
   const handleCreateEvent = () => {
-    setIsCreating(true);
+    navigate("/events/create")
   };
-
-  const goBack = () => setIsCreating(false);
-
-  const navigate = useNavigate();
   
   if (isCreating) {
     return (
       <div className="flex flex-col items-center h-screen-h">
-        <AddEventForm postEvent={postEvent} />
-        <BottomButton onClick={goBack} text="Back to events" />
+        <EventForm event={defaultEventListing} updating={false} />
+        <BottomButton onClick={() => console.log()} text="Back to events" />
       </div>
     );
   }
@@ -82,7 +45,7 @@ const {user} = useClerk();
       <ul className="flex flex-wrap gap-10">
         {events.map((event) => (
           <li key={event.id}>
-            <EditEventCard event={event} onDelete={deleteEvent} onUpdate={handleCreateEvent}/>
+            <EditEventCard event={event}/>
           </li>
         ))}
       </ul>
