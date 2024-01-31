35c49550a4cb296dfc9b5a0f625c49be664c142d
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-31 11:18:28 +0100

    add input components to form

--- frontend/src/components/AddEventForm.tsx
+++ frontend/src/components/AddEventForm.tsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { EventListing, defaultEventListing } from "../util";
-import PrimaryButton from "./PrimaryButton";
 import MapWindow from "./MapWindow";
+import TextInput from "./inputs/TextInput";
+import DateTimeInput from "./inputs/DateTimeInput";
+import TextArea from "./inputs/TextArea";
 
 interface EventFormProps {
   postEvent: (event: EventListing) => Promise<void>;
 }
 
 const AddEventForm: React.FC<EventFormProps> = ({ postEvent }) => {
-  const [position, setPosition] = useState({lat: 59.34676644462517, lng: 18.055573862709853});
+  const [position, setPosition] = useState({
+    lat: 59.34676644462517,
+    lng: 18.055573862709853,
+  });
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -26,13 +31,13 @@ const AddEventForm: React.FC<EventFormProps> = ({ postEvent }) => {
   const [eventListing, setEventListing] =
     useState<EventListing>(defaultEventListing);
 
-    useEffect(() => {
-      setEventListing(listing => ({
-        ...listing,
-        latitude: position.lat,
-        longitude: position.lng
-      }));
-    }, [position]);
+  useEffect(() => {
+    setEventListing((listing) => ({
+      ...listing,
+      latitude: position.lat,
+      longitude: position.lng,
+    }));
+  }, [position]);
 
   const handleChange = (
     event:
@@ -56,119 +61,25 @@ const AddEventForm: React.FC<EventFormProps> = ({ postEvent }) => {
   return (
     <form onSubmit={handleSubmit} className="flex items-center h-80vh">
       <section className="flex flex-col items-center h-full">
-      <h1>Create new event:</h1>
-      <div>
-        <label>
-          <div className="label">
-            <span className="label-text">Title:</span>
-          </div>
-          <input
-            type="text"
-            name="title"
-            className="input input-bordered input-ghost"
-            value={eventListing.title}
-            onChange={handleChange}
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          <div className="label">
-            <span className="label-text">Description:</span>
-          </div>
-          <textarea
-            name="description"
-            className="textarea textarea-bordered"
-            value={eventListing.description}
-            onChange={handleChange}
-          />
-        </label>
-      </div>
-      <div>
-            <h1>Start time:</h1>
-
-          <input
-            type="datetime-local"
-            name="startTime"
-            className="input input-bordered input-ghost"
-            value={eventListing.startTime.toString()}
-            onChange={handleChange}
-            required
-          />
-
-            <h1>End time:</h1>
-
-            <input
-            type="datetime-local"
-            name="endTime"
-            className="input input-bordered input-ghost"
-            value={eventListing.endTime.toString()}
-            onChange={handleChange}
-            required
-          />
-        <label>
-          <div className="label">
-            <span className="label-text">Latitude:</span>
-          </div>
-          <input
-            type="text"
-            name="latitude"
-            className="input input-bordered input-ghost"
-            value={position.lat}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          <div className="label">
-            <span className="label-text">Longitude:</span>
-          </div>
-          <input
-            type="text"
-            name="longitude"
-            className="input input-bordered input-ghost"
-            value={position.lng}
-            onChange={handleChange}
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <label className="form-control w-full max-w-xs">
-          <div className="label">
-            <span className="label-text">Price:</span>
-          </div>
-          <input
-            type="number"
-            name="price"
-            className="input input-bordered"
-            value={eventListing.price.toString()}
-            onChange={handleChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label className="form-control w-full max-w-xs">
-          <div className="label">
-            <span className="label-text">Category:</span>
-          </div>
-          <input
-            type="text"
-            name="category"
-            className="input input-bordered"
-            value={eventListing.category}
-            onChange={handleChange}
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <PrimaryButton text="Submit" onClick={() => handleSubmit} />
-      </div>
+      <div className="card w-96 bg-neutral text-neutral-content">
+  <div className="card-body">
+    <h2 className="card-title">Create new event:</h2>
+    <TextInput title="Title:" name="title" value={eventListing.title} onChange={handleChange}/>
+        <TextArea title="Desctiption" name="description" value={eventListing.description} onChange={handleChange}/>
+        <DateTimeInput title="Start time:" name="startTime" value={eventListing.startTime} onChange={handleChange}/>
+        <DateTimeInput title="End time:" name="endTime" value={eventListing.endTime} onChange={handleChange}/>
+        <TextInput title="Latitude:" name="latitude" value={eventListing.latitude} onChange={handleChange} hidden={true}/>
+        <TextInput title="Longitude:" name="longitude" value={eventListing.longitude} onChange={handleChange} hidden={true}/>
+        <TextInput title="Price:" name="price" value={eventListing.price} onChange={handleChange}/>
+        <TextInput title="Category:" name="category" value={eventListing.category} onChange={handleChange}/>
+    <div className="card-actions justify-end">
+        <button className="btn btn-primary" onClick={() => handleSubmit}>Submit</button> 
+    </div>
+  </div>
+</div>
       </section>
       <section className="w-40vh h-40vh">
-      <MapWindow position={position} setPosition={setPosition}/>
+        <MapWindow position={position} setPosition={setPosition} />
       </section>
     </form>
   );
