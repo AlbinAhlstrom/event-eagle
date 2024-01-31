9c5a0859a577502ea98b313294edefac2b530d24
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-01-31 18:35:34 +0100

    fix fetching

---
+++ frontend/src/pages/EditEvent.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import EventForm from '../components/EventForm';
+import { EventListing } from '../util';
+
+const EditEvent = () => {
+  const { id } = useParams<'id'>();
+  const [eventData, setEventData] = useState(null);
+  const navigate = useNavigate();
+
+  const BASE_URL = 'https://event-eagle.azurewebsites.net';
+  const EVENTS_ENDPOINT = `${BASE_URL}/Events/${id}`;
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      try {
+        const response = await fetch(EVENTS_ENDPOINT);
+        if (!response.ok) {
+          throw new Error('Event not found');
+        }
+        const data = await response.json();
+        setEventData(data);
+      } catch (error) {
+        console.error('Failed to fetch event', error);
+      }
+    };
+
+    fetchEvent();
+  }, [EVENTS_ENDPOINT]);
+
+  const updateEvent = async (eventData: EventListing) => {
+    console.log('put here:', eventData);
+    try {
+      const response = await fetch(EVENTS_ENDPOINT, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(eventData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update event');
+      }
+      navigate('/admin');
+    } catch (error) {
+      console.error('Failed to update event', error);
+    }
+  };
+
+  // Only display the form if eventData is present
+  return eventData ? (
+    <EventForm event={eventData} onFormSubmit={updateEvent} />
+  ) : (
+    <p>Loading event...</p>
+  );
+};
+
+export default EditEvent;
