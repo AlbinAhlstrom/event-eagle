7936f1ffe867bca77951d9b73dac4c6dabe0bd41
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-01 15:19:42 +0100

    upadet to use real event listing

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,12 +1,11 @@
-import { FormFields } from '../components/EventForm';
-import { defaultEventListing } from '../util';
+import { EventListing } from '../util';
 import EventForm from '../components/EventForm';
 import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
   const navigate = useNavigate()
 
-  const postEvent = (event: FormFields) => {
+  const postEvent = (event: EventListing) => {
     console.log(event)
     // const BASE_URL = "http://event-eagle.azurewebsites.net"
     // const EVENTS_ENDPOINT = `${BASE_URL}/Events`;
@@ -25,7 +24,7 @@ const Create = () => {
 
     return (
         <div className="flex flex-col items-center h-screen-h">
-          <EventForm onSave={postEvent} defaultEvent={defaultEventListing} />
+          <EventForm onSave={postEvent} />
           <button className='btn btn-primary fixed bottom-2 mt-auto self-center' onClick={() => navigate("/admin")}>Back to dashboard</button>
         </div>
       )
