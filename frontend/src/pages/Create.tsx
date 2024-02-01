e012fdb6d3198b52934f0d6e79b6ad25bf754275
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-01 14:59:53 +0100

    import wrong event listing

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,4 +1,4 @@
-import { EventListing, defaultEventListing } from '../util';
+import { EventListing } from '../components/EventForm';
 import EventForm from '../components/EventForm';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,24 +6,25 @@ const Create = () => {
   const navigate = useNavigate()
 
   const postEvent = async (event: Omit<EventListing, "id">) => {
-    const BASE_URL = "http://event-eagle.azurewebsites.net"
-    const EVENTS_ENDPOINT = `${BASE_URL}/Events`;
-    const response = await fetch(EVENTS_ENDPOINT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(event),
-    });
-    const data: EventListing = await response.json();
-    console.log(data)
-    navigate("/admin");
+    console.log(event)
+    // const BASE_URL = "http://event-eagle.azurewebsites.net"
+    // const EVENTS_ENDPOINT = `${BASE_URL}/Events`;
+    // const response = await fetch(EVENTS_ENDPOINT, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(event),
+    // });
+    // const data: EventListing = await response.json();
+    // console.log(data)
+    // navigate("/admin");
 
   };
 
     return (
         <div className="flex flex-col items-center h-screen-h">
-          <EventForm defaultEvent={defaultEventListing} onSave={postEvent} />
+          <EventForm onSave={postEvent} />
           <button className='btn btn-primary fixed bottom-2 mt-auto self-center' onClick={() => navigate("/admin")}>Back to dashboard</button>
         </div>
       )
