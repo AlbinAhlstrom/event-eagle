9b0dc01b4e15a48918eaeb94bee52d1160b70578
ayoubzann <ayoubzann@gmail.com>
2024-02-01 14:20:35 +0100

    Added styling fix

--- frontend/src/components/EventDetailsCard.tsx
+++ frontend/src/components/EventDetailsCard.tsx
@@ -11,7 +11,7 @@ const EventDetailsCard: React.FC<EventDetailsCardProps> = ({ event }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="hero-content text-center text-neutral-content bg-base-100 rounded-2xl p-10">
+    <div className="hero-content text-center drop-shadow-2xl text-neutral-content bg-base-100 rounded-2xl p-10">
       <div className="max-w-md">
         <h1 className="mb-5 text-5xl p-3 font-bold">{event.title}</h1>
 
@@ -21,7 +21,7 @@ const EventDetailsCard: React.FC<EventDetailsCardProps> = ({ event }) => {
         <h2 className="my-6 text-xl">Price: {event.price} SEK</h2>
         <p>{event.address}</p>
         <button
-          className="btn btn-primary m-5 p-5"
+          className="btn btn-primary"
           onClick={() => navigate("purchase/")}
         >
           Purchase
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
       appearance={{ 
-        baseTheme: dark,
+        baseTheme: dark
     }}
     >
       <BrowserRouter>
