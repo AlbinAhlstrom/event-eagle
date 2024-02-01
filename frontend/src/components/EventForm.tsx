ffbf2a38968faedd7bdaac558f71454b0dca84d3
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-01 15:20:13 +0100

    remove FormField for EventListing

--- frontend/src/components/EventForm.tsx
+++ frontend/src/components/EventForm.tsx
@@ -1,35 +1,49 @@
-import {useForm} from "react-hook-form"
+import { useForm } from "react-hook-form";
+import { EventListing } from "../util";
 import { defaultEventListing } from "../util";
 import MapWindow from "./MapWindow";
 export interface FormFields {
-  title: string
+  title: string;
 }
 
 type props = {
-  onSave: (formData: FormFields) => void
-  defaultEvent: FormFields
-}
+  onSave: (formData: EventListing) => void;
+  defaultEvent: EventListing;
+};
 
-const EventForm = ({ onSave, defaultEvent = defaultEventListing}: props) => {
-  const { register, control, handleSubmit } = useForm({defaultValues:defaultEvent})
+const EventForm = ({ onSave, defaultEvent = defaultEventListing }: props) => {
+  const { register, control, handleSubmit } = useForm({
+    defaultValues: defaultEvent,
+  });
 
-  const handleSave = async (formValues: FormFields) => {
+  const handleSave = async (formValues: EventListing) => {
     await onSave(formValues);
   };
 
   return (
-    <form onSubmit={handleSubmit(handleSave)} className="flex items-center h-80vh">
-        <div className="card w-96 bg-neutral text-neutral-content">
-          <div className="card-body">
-            <h2 className="card-title">Create new event:</h2>
-            <input {...register("title")}/>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary" type="submit">
-                Submit
-              </button>
-            </div>
+    <form
+      onSubmit={handleSubmit(handleSave)}
+      className="flex items-center h-80vh"
+    >
+      <div className="card w-96 bg-neutral text-neutral-content">
+        <div className="card-body">
+          <h2 className="card-title">Create new event:</h2>
+          <div>
+            <p>Title</p>
+            <input {...register("title")} />
+          </div>
+          <div>
+            <p>Description</p>
+            <textarea {...register("description")} />
+          </div>
+        
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
           </div>
         </div>
+      </div>
     </form>
   );
 };
