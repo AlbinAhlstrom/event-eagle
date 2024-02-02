01aefbfec0d74cea8e83fc87a9b21b47da23b186
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-02 11:01:53 +0100

    working state

--- frontend/src/components/EventForm.tsx
+++ frontend/src/components/EventForm.tsx
@@ -2,20 +2,21 @@ import { useForm, useController } from "react-hook-form";
 import { EventListing } from "../util";
 import { defaultEventListing } from "../util";
 import { categoryType, categories } from "../util";
-import Select from "react-select"
+import Select from "react-select";
 import MapWindow from "./MapWindow";
+import "flatpickr/dist/themes/material_green.css";
 
 const categoryOptions = [
   { value: categories.music, label: categories.music },
   { value: categories.sports, label: categories.sports },
   { value: categories.arts, label: categories.arts },
   { value: categories.family, label: categories.family },
-] as const
+] as const;
 
 type categoryOption = {
-  value: categoryType
-  label: categoryType
-}
+  value: categoryType;
+  label: categoryType;
+};
 
 type props = {
   onSave: (formData: EventListing) => void;
@@ -23,18 +24,19 @@ type props = {
 };
 
 const EventForm = ({ onSave, defaultEvent = defaultEventListing }: props) => {
-  const { register, control, handleSubmit } = useForm({
+  const { register, control, handleSubmit, setValue } = useForm({
     defaultValues: defaultEvent,
   });
 
-  const { field } = useController({name: "category", control})
-
-  const handleFieldChange = (newValue?: categoryOption ) => {
-      field.onChange(newValue? newValue.value : categoryOptions[0]);
-    }
-
   const handleSave = async (formValues: EventListing) => {
     await onSave(formValues);
+  };
+
+
+  const { field } = useController({ name: "category", control });
+
+  const handleFieldChange = (newValue?: categoryOption) => {
+    field.onChange(newValue ? newValue.value : categoryOptions[0]);
   };
 
   return (
@@ -44,36 +46,31 @@ const EventForm = ({ onSave, defaultEvent = defaultEventListing }: props) => {
     >
       <div className="card w-96 bg-neutral text-neutral-content">
         <div className="card-body">
-          <h2 className="card-title">Create new event:</h2>
-          <div>
-            <p>Title</p>
-            <input {...register("title")} />
-          </div>
-          <div>
-            <p>Description</p>
-            <textarea {...register("description")} />
-          </div>
-          <div>
-            <p>Category</p>
-            <Select 
-              value={categoryOptions.find((option) => option.value === field.value)}
-              onChange={handleFieldChange}
-              options={categoryOptions}
-            />
-          </div>
+          <label>
+            <div className="label">
+              <span className="label-text">{"title"}</span>
+            </div>
+            <input className="input input-bordered" {...register("title")} />
+          </label>
+          <label>
+            <div className="label">
+              <span className="label-text">{"description"}</span>
+            </div>
+            <textarea className="textarea textarea-bordered" {...register("description")} />
+          </label>
+
           <div className="card-actions justify-end">
             <button className="btn btn-primary" type="submit">
               Submit
             </button>
           </div>
           <div>
-            <p>Start Time</p>
-            <input type="datetime-local" {...register("startTime")} />
-          </div>
-          <div>
-            <p>End Time</p>
-            <input type="datetime-local" {...register("endTime")} />
-          </div>
+          <p>Start Time</p>
+          <input
+            type="datetime-local"
+            {...register("startTime")}
+          />
+        </div>
           <div>
             <p>Venue</p>
             <input {...register("venue")} />
@@ -83,16 +80,18 @@ const EventForm = ({ onSave, defaultEvent = defaultEventListing }: props) => {
             <input {...register("address")} />
           </div>
           <div>
-            <p>Latitude</p>
-            <input type="number" {...register("latitude")} />
-          </div>
-          <div>
-            <p>Longitude</p>
-            <input type="number" {...register("longitude")} />
-          </div>
-          <div>
             <p>Price</p>
             <input type="number" {...register("price")} />
+          </div>
+          <div>
+            <p>Category</p>
+            <Select
+              value={categoryOptions.find(
+                (option) => option.value === field.value
+              )}
+              onChange={handleFieldChange}
+              options={categoryOptions}
+            />
           </div>
         </div>
       </div>
