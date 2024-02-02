6b19f9545ce11a30e0415637f46bd22d17fb4dca
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-02 15:44:24 +0100

    add useEffect

--- frontend/src/pages/Events.tsx
+++ frontend/src/pages/Events.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useSWR from "swr";
 import { useParams, useNavigate } from "react-router-dom";
 import EventCard from "../components/EventCard";
