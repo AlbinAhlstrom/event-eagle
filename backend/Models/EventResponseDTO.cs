c28eaca071420b5678abe8fb01e31f8897cb79f8
ayoubzann <ayoubzann@gmail.com>
2024-02-02 15:53:56 +0100

    Abstracted out class from file

--- backend/Models/EventResponseDTO.cs
+++ backend/Models/EventResponseDTO.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 
-namespace Data;
+namespace Models;
 public class EventResponseDTO
 {
     public int Id { get; set; }
@@ -10,27 +10,9 @@ public class EventResponseDTO
     public Details Details { get; set; }
     public Location Location { get; set; }
     public ActionOption[] ActionOptions { get; set; }
-
 }
 
-public class Details
-{
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
-    public decimal Price { get; set; }
-}
 
-public class Location
-{
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public string Venue { get; set; }
-    public string Address { get; set; }
-}
 
-public class ActionOption
-{
-    public string Name { get; set; }
-    public string Uri { get; set; }
-    public string Method { get; set; }
-}
+
+
