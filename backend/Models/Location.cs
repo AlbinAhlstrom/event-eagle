cc9aa512ca490df0a2d54567f6b25c44eb120284
ayoubzann <ayoubzann@gmail.com>
2024-02-02 15:54:00 +0100

    Abstracted out class from file

---
+++ backend/Models/Location.cs
@@ -0,0 +1,8 @@
+namespace Models;
+public class Location
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Venue { get; set; }
+    public string Address { get; set; }
+}
