24890adeb07a3183d03e181bf18e0939489c9cc2
ayoubzann <ayoubzann@gmail.com>
2024-02-02 16:42:52 +0100

    added EventId to DTO

--- backend/Models/EventResponseDTO.cs
+++ backend/Models/EventResponseDTO.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 namespace Models;
 public class EventResponseDTO
 {
+    public EventId eventId {get; set;}
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
