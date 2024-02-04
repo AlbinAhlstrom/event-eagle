ce45eb7fce7864130f8ce24918b85060ce848aeb
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-04 21:21:18 +0100

    center button

--- frontend/src/pages/EditEvent.tsx
+++ frontend/src/pages/EditEvent.tsx
@@ -55,10 +55,10 @@ const EditEvent = () => {
 
   // Only display the form if eventData is present
   return eventData ? (
-    <div className='div className="flex flex-col justify-center items-center h-screen-h"'>
+    <div className='flex flex-col justify-between items-center h-screen-h'>
     <EventForm defaultEvent={eventData} onSave={updateEvent} title="Update event:"/>
-    <button className='btn btn-primary fixed bottom-2 mt-auto self-center' onClick={() => navigate("/admin")}>Back to dashboard</button>
-    </div>
+    <button className='btn btn-primary fixed bottom-2 mt-auto mx-auto' onClick={() => navigate("/admin")}>Back to dashboard</button>
+</div>
   ) : (
     <p>Loading event...</p>
   );
