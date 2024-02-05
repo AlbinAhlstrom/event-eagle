9aa7f5c606e0a70313d3ee98535e8faf6832dffe
ayoubzann <ayoubzann@gmail.com>
2024-02-05 11:22:35 +0100

    Created table

---
+++ backend/Models/UserEvents.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Models;
+
+namespace Models;
+
+public class UserEvents
+{
+    [Key]
+    public int Id { get; set; }
+    public string UserId {get; set;}
+    
+    public int EventId { get; set; }
+    public Event Event {get; set;}
+
+    public bool CreatedByUser {get; set;} = false;
+}
