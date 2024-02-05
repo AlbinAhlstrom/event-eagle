e6e18383895fd48afc4000cbb9d971b35d839be9
ayoubzann <ayoubzann@gmail.com>
2024-02-05 14:46:23 +0100

    Finished page

--- frontend/src/pages/SavedEvents.tsx
+++ frontend/src/pages/SavedEvents.tsx
@@ -1,38 +1,49 @@
 import React, { useEffect, useState } from "react";
 import EventCard from "../components/EventCard";
+import { useClerk } from "@clerk/clerk-react";
+import { SavedEvent } from "../util";
 
-const SavedEvents = () => {
-  const [savedEvents, setSavedEvents] = useState([]);
+const SavedEvents: React.FC = () => {
+  const [savedEvents, setSavedEvents] = useState<SavedEvent[]>([]);
+  const {user} = useClerk();
 
   const fetchUserEvents = async () => {
     const response = await fetch(
-      "https://event-eagle.azurewebsites.net/Events/userEvents"
+      `https://event-eagle.azurewebsites.net/Events/userEvents?userId=${user?.id}`,
     );
     const data = await response.json();
     setSavedEvents(data);
   };
+  const updateSavedEvents = async () => {
+    await fetchUserEvents();
+  };
 
   useEffect(() => {
     fetchUserEvents();
-    console.log(savedEvents);
   }, []);
 
+
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center mt-10">
+      <h1 className="text-5xl font-bold">Saved Events</h1>
+      <div className="flex gap-10 flex-wrap justify-center mt-10 ">
+
       {savedEvents.map((ev, index) => {
         return (
           <EventCard
           key={index}
-            id={ev.event.id}
-            title={ev.event.title}
-            description={ev.event.description}
-            startTime={ev.event.startTime}
-            venue={ev.event.venue}
-            price={ev.event.price}
-            category={ev.event.category}
+          id={ev.event.id}
+          title={ev.event.title}
+          description={ev.event.description}
+          startTime={ev.event.startTime}
+          venue={ev.event.venue}
+          price={ev.event.price}
+          category={ev.event.category}
+          updateSavedEvents={updateSavedEvents}
           />
-        );
-      })}
+          );
+        })}
+        </div>
     </div>
   );
 };
