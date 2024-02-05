a6aee25e3b7934c6aaed5c53880b3e298763fc2f
ayoubzann <ayoubzann@gmail.com>
2024-02-05 11:23:14 +0100

    Added DbSet

--- backend/Data/eventContext.cs
+++ backend/Data/eventContext.cs
@@ -9,5 +9,6 @@ public class EventContext : DbContext
         }
 
         public DbSet<Event> Events { get; set; } = default!;
+        public DbSet<UserEvents> UserEvents { get; set; } = default!;
 }
 }
