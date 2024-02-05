ecd062eaea077141a48c0df437b67cb129c16909
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-05 12:39:28 +0100

    add base map

--- frontend/src/components/DiscoveryMap.tsx
+++ frontend/src/components/DiscoveryMap.tsx
@@ -6,16 +6,9 @@ type MapWindowProps =  {
   center: Coordinate
   events: EventListing[]
 }
-const MapWindow: React.FC<MapWindowProps> = ({center, events}) => {
+const MapWindow: React.FC<MapWindowProps> = ({center, events}: MapWindowProps) => {
   const apiUrl = import.meta.env.VITE_GMAPS_KEY;
   const mapId = import.meta.env.VITE_GMAPS_MAPID;
-  
-  const handleDragEnd = (event: google.maps.MapMouseEvent) => {
-    const newPos = event.latLng;
-    if (newPos) {
-      setPosition({lat: newPos.lat(), lng: newPos.lng()});
-    }
-  };
 
   return (
     <div className="h-60vh w-80vh rounded">
@@ -23,17 +16,19 @@ const MapWindow: React.FC<MapWindowProps> = ({center, events}) => {
         <Map
         className="h-full w-full rounded-xl"
           zoom={16}
-          center={position}
+          center={center}
           mapId={mapId}
           gestureHandling={'greedy'}
           disableDefaultUI={false}>          
-          <AdvancedMarker
+          {/* <AdvancedMarker
             position={position}
             draggable={true}
             onDragEnd={handleDragEnd}
           >
-            <Pin />
+          <Pin />
           </AdvancedMarker>
+          */}
+            
         </Map>
       </APIProvider>
     </div>
