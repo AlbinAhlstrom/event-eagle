01fea88297e79594d6c87241f078200aae110f3e
ayoubzann <ayoubzann@gmail.com>
2024-02-05 12:11:32 +0100

    Refactor

--- frontend/src/pages/SavedEvents.tsx
+++ frontend/src/pages/SavedEvents.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const SavedEvents = () => {
+
+  const fetchUserEvents = async () => {
+
+    const response = await fetch("https://event-eagle.azurewebsites.net/Events/userEvents");
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+useEffect(()=>{
+  fetchUserEvents();
+},[])
   return (
     <div>SavedEvents</div>
   )
