85daeb134a1b15616379bcb41bbcf0f6da8b170d
ayoubzann <ayoubzann@gmail.com>
2024-02-05 12:58:27 +0100

    Added fetch and map

--- frontend/src/pages/SavedEvents.tsx
+++ frontend/src/pages/SavedEvents.tsx
@@ -1,21 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
+import EventCard from "../components/EventCard";
 
 const SavedEvents = () => {
+  const [savedEvents, setSavedEvents] = useState([]);
 
   const fetchUserEvents = async () => {
-
-    const response = await fetch("https://event-eagle.azurewebsites.net/Events/userEvents");
+    const response = await fetch(
+      "https://event-eagle.azurewebsites.net/Events/userEvents"
+    );
     const data = await response.json();
+    setSavedEvents(data);
+  };
 
-    console.log(data);
-  }
+  useEffect(() => {
+    fetchUserEvents();
+    console.log(savedEvents);
+  }, []);
 
-useEffect(()=>{
-  fetchUserEvents();
-},[])
   return (
-    <div>SavedEvents</div>
-  )
-}
+    <div>
+      {savedEvents.map((ev, index) => {
+        return (
+          <EventCard
+          key={index}
+            id={ev.event.id}
+            title={ev.event.title}
+            description={ev.event.description}
+            startTime={ev.event.startTime}
+            venue={ev.event.venue}
+            price={ev.event.price}
+            category={ev.event.category}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-export default SavedEvents
+export default SavedEvents;
