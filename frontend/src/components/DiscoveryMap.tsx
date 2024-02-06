ad778f4fb2f21de1573da95e8aa5b55493daedd2
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-06 15:05:46 +0100

    map in working state

--- frontend/src/components/DiscoveryMap.tsx
+++ frontend/src/components/DiscoveryMap.tsx
@@ -1,33 +1,49 @@
 import { useEffect } from 'react';
-import { Coordinate, Event } from '../util';
-import { LoadScript, GoogleMap } from '@react-google-maps/api';
+import GoogleMap from 'google-map-react';
 
+type Coordinate = {
+  lat: number;
+  lng: number;
+};
 
-type MapWindowProps =  {
-  center: Coordinate
-  distanceFilter: number
-}
+type MapWindowProps = {
+  center: Coordinate;
+  distanceFilter: number;
+};
 
-const MapComponent = ({center, distanceFilter}: MapWindowProps) => {
+const MapComponent = ({ center, distanceFilter }: MapWindowProps) => {
+
   const mapStyles = {
     height: "70vh",
     width: "70vh",
   };
 
-  useEffect(() => {}, [center])
+  useEffect(() => {}, [center]);
 
-  if (center.lat === 0) return <div>Loading</div>
+  const Map = () =>(
+    <div style={mapStyles}>
+      <GoogleMap
+        bootstrapURLKeys={{ key: import.meta.env.VITE_GMAPS_KEY }}
+        defaultZoom={16}
+        defaultCenter={center}
+        onGoogleApiLoaded={({map, maps}) =>
+          new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.3,
+            map,
+            center: center,
+            radius: 275,
+          })}
+      />
+    </div>
+  );
 
   return (
-    <LoadScript googleMapsApiKey={import.meta.env.VITE_GMAPS_KEY}>
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={16 - distanceFilter}
-        center={center}
-      >
-      </GoogleMap>
-    </LoadScript>
+    Map()
   );
-}
+};
 
 export default MapComponent;
