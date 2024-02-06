eff533a7775206d73f54adfb2b82c84c31e4f272
Muhammad Zulqarnain Aslam <147504664+Zulqarnain94@users.noreply.github.com>
2024-02-06 11:19:55 +0100

    Added PaymentController Controller

---
+++ backend/Controllers/PaymentController.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Stripe;
+using Stripe.Checkout;
+using Models; 
+using System;
+using System.Collections.Generic;
+
+
+namespace Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+
+
+
+  public class PaymentController : ControllerBase
+  {
+    private readonly StripeSettings _stripeSettings;
+
+    public PaymentController(IOptions<StripeSettings> stripeSettings)
+    {
+      _stripeSettings = stripeSettings.Value;
+      StripeConfiguration.ApiKey = _stripeSettings.SecretKey;
+    }
+
+    [HttpPost("create-checkout-session")]
+    public IActionResult CreateCheckoutSession([FromBody] PaymentRequest paymentRequest)
+    {
+      try
+      {
+        var currency = "sek";
+        var successUrl = "http://localhost:3000/success";
+        var cancelUrl = "http://localhost:3000/cancel";
+
+        var options = new SessionCreateOptions
+        {
+          PaymentMethodTypes = new List<string> { "card" },
+          LineItems = new List<SessionLineItemOptions>
+                    {
+                        new SessionLineItemOptions
+                        {
+                            PriceData = new SessionLineItemPriceDataOptions
+                            {
+                                Currency = currency,
+                                UnitAmount = paymentRequest.Amount * 100,  // Amount in cents
+                                ProductData = new SessionLineItemPriceDataProductDataOptions
+                                {
+                                    Name = "Product Name",
+                                    Description = "Product Description"
+                                }
+                            },
+                            Quantity = 1
+                        }
+                    },
+          Mode = "payment",
+          SuccessUrl = successUrl,
+          CancelUrl = cancelUrl
+        };
+
+        var service = new SessionService();
+        var session = service.Create(options);
+
+        return Ok(new { sessionId = session.Id });
+      }
+      catch (Exception ex)
+      {
+
+        Console.WriteLine($"Exception: {ex.Message}");
+        return StatusCode(500, "Internal Server Error");
+      }
+    }
+  }
+}
