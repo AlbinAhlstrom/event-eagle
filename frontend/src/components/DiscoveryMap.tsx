97d0fd541951b939568beed2794f35686659e3ff
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-06 14:30:46 +0100

    stop using vis.gl maps

--- frontend/src/components/DiscoveryMap.tsx
+++ frontend/src/components/DiscoveryMap.tsx
@@ -1,41 +1,33 @@
-import { APIProvider, Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
+import { useEffect } from 'react';
 import { Coordinate, Event } from '../util';
+import { LoadScript, GoogleMap } from '@react-google-maps/api';
 
 
 type MapWindowProps =  {
-  events: Event[]
   center: Coordinate
   distanceFilter: number
 }
 
-const MapWindow: React.FC<MapWindowProps> = ({events, center, distanceFilter}: MapWindowProps) => {
-  const apiUrl = import.meta.env.VITE_GMAPS_KEY;
-  const mapId = import.meta.env.VITE_GMAPS_MAPID;
+const MapComponent = ({center, distanceFilter}: MapWindowProps) => {
+  const mapStyles = {
+    height: "70vh",
+    width: "70vh",
+  };
+
+  useEffect(() => {}, [center])
+
+  if (center.lat === 0) return <div>Loading</div>
 
   return (
-    
-    <div className="relative h-60vh w-60vh rounded">
-      <h1 className='hidden'>{events.toString()}</h1>
-      <h1 className='hidden'>{distanceFilter}</h1>
-      <APIProvider apiKey={apiUrl}>
-        <Map
-          className="h-full w-full rounded-xl"
-          zoom={16 - distanceFilter}
-          center={center}
-          mapId={mapId}
-          gestureHandling={'greedy'}
-          disableDefaultUI={true}
-        >
-          <AdvancedMarker position={center} draggable={false}>
-            <Pin />
-          </AdvancedMarker>
-        </Map>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="h-1/2 w-1/2 bg-yellow-500 opacity-50 rounded-full"></div>
-        </div>
-      </APIProvider>
-    </div>
+    <LoadScript googleMapsApiKey={import.meta.env.VITE_GMAPS_KEY}>
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={16 - distanceFilter}
+        center={center}
+      >
+      </GoogleMap>
+    </LoadScript>
   );
-};
+}
 
-export default MapWindow;
+export default MapComponent;
