1308703df50cb805d95dfbf6d2d29c0ae4d548b2
ayoubzann <ayoubzann@gmail.com>
2024-02-07 12:02:34 +0100

    Added ticketId fix

--- backend/Models/Ticket.cs
+++ backend/Models/Ticket.cs
@@ -1,19 +1,28 @@
 using System.ComponentModel.DataAnnotations;
-using Microsoft.EntityFrameworkCore;
 
-namespace Models;
-
-public class Ticket
+namespace Models
 {
-    public string TicketId { get; set; } = new TicketId().ToString();
-    [Key]
-    public int Id {get; set;}
-    
-    public int EventId { get; set; }
-    public Event Event { get; set; }
-    public string SellerId { get; set; }
-    public string SellerName { get; set; }
-    public bool Available { get; set; }
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
 
+        public string TicketId { get; set; }
+
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+        public string SellerId { get; set; }
+        public string SellerName { get; set; }
+        public bool Available { get; set; }
+
+        public Ticket()
+        {
+            TicketId = $"ti-{GenerateShortGuid()}";
+        }
+
+        private static string GenerateShortGuid()
+        {
+            return Guid.NewGuid().ToString("N").Substring(0, 10);
+        }
+    }
 }
-
