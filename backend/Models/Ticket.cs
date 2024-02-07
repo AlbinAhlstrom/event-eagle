af7af91732ecc4630484c3cc282093921d71ffda
ayoubzann <ayoubzann@gmail.com>
2024-02-07 11:22:58 +0100

    Added int id

--- backend/Models/Ticket.cs
+++ backend/Models/Ticket.cs
@@ -1,14 +1,19 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Models;
 
 public class Ticket
 {
+    public string TicketId { get; set; } = new TicketId().ToString();
     [Key]
-    public TicketId Id { get; set; }
+    public int Id {get; set;}
+    
     public int EventId { get; set; }
     public Event Event { get; set; }
     public string SellerId { get; set; }
     public string SellerName { get; set; }
     public bool Available { get; set; }
+
 }
+
