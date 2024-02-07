9172246ec528c47dbf5c5e4b462366bd96a93d72
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-07 23:22:42 +0100

    set radius

--- frontend/src/pages/EventsMap.tsx
+++ frontend/src/pages/EventsMap.tsx
@@ -38,7 +38,7 @@ const EventsMap: React.FC = () => {
         onChange={handleSliderChange}
       />
       <div className="flex justify-center items-center flex-wrap gap-10 mt-4">
-        <DiscoveryMap center={userLocation} />
+        <DiscoveryMap center={userLocation} circleRadius={distanceFilter * 1000}/>
       </div>
       <button
         className="btn btn-primary self-center fixed bottom-2 mt-auto w-40 z-50 max-md:w-5/6"
