a489edb508947239cb8a4c3f425fd06dd04e3b8c
ayoubzann <ayoubzann@gmail.com>
2024-02-07 10:04:04 +0100

    Moved file - Restructure

--- backend/Models/Location.cs
+++
@@ -1,8 +0,0 @@
-namespace Models;
-public class Location
-{
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public string Venue { get; set; }
-    public string Address { get; set; }
-}
