57ea4edab4d0d20a4a8703f1334103b53ac9caa8
ayoubzann <ayoubzann@gmail.com>
2024-02-08 10:47:39 +0100

    Update DB

---
+++ backend/Migrations/20240208093442_PurchasedBy.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class PurchasedBy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PurchasedBy",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchasedBy",
+                table: "Tickets");
+        }
+    }
+}
