901925eaac938c76627b1bd000d9f61d6e0c0635
AlbinAhlstrom <103491746+AlbinAhlstrom@users.noreply.github.com>
2024-02-08 06:53:35 +0100

    replace lat and long with coordinates

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -15,24 +15,12 @@ export const fromDateTimeString = (dateTimeString: string): Date => {
   return new Date(year, month - 1, day, hours, minutes);
 };
 
-export const getDistanceFromLatLonInKm = (
-  lat1: number,
-  lon1: number,
-  lat2: number,
-  lon2: number
-): number => {
-  // Radius of the earth in km
+export const getDistanceFromLatLonInKm = (pos1: Coordinate, pos2: Coordinate)=> {
   const R = 6371;
-  const dLat = deg2rad(lat2 - lat1);
-  const dLon = deg2rad(lon2 - lon1);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  // Distance in km
+  const dLat = deg2rad(pos2.lat - pos1.lat);
+  const dLon = deg2rad(pos2.lng - pos1.lng);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(pos1.lat)) * Math.cos(deg2rad(pos2.lat)) 
+    * Math.sin(dLon / 2) * Math.sin(dLon / 2);const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const d = R * c;
   return d;
 };
