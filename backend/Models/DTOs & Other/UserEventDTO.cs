3a6b4391cec3aaf0a26913280a0e1b83eea43413
ayoubzann <ayoubzann@gmail.com>
2024-02-08 10:46:50 +0100

    Formatted page

--- backend/Models/DTOs & Other/UserEventDTO.cs
+++ backend/Models/DTOs & Other/UserEventDTO.cs
@@ -1,7 +1,8 @@
 namespace Models;
 
-public class UserEventDTO{
+public class UserEventDTO
+{
     public string UserId { get; set; }
-    public int EventId {get; set;}
-    public bool CreatedByUser {get; set;}
+    public int EventId { get; set; }
+    public bool CreatedByUser { get; set; }
 }
