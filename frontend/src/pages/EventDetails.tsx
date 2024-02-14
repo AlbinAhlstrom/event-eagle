5906093fac7b052551ab4f8475cf02f81fbf058e
ayoubzann <ayoubzann@gmail.com>
2024-02-14 16:19:15 +0100

    changed bg

--- frontend/src/pages/EventDetails.tsx
+++ frontend/src/pages/EventDetails.tsx
@@ -15,7 +15,7 @@ const EventDetails = () => {
       <div
         className="hero min-h-screen-h"
         style={{
-          backgroundImage: `url(${image})`,
+          backgroundImage: `url(https://cdn.sanity.io/images/fvrrd1kn/production/5b37976bf453b25395a1caad66250dedfd7cb7a5-2400x1600.jpg?q=75&fit=clip&auto=format)`,
         }}
       >
         <span className="h-2/6 w-2/6 absolute top-20"></span>
